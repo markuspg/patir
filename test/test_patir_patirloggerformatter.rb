c926d091cba2f98d140e72c215fe0276f47744dd
markuspg <markuspg@users.noreply.github.com>
2021-01-19 22:08:58 +0100

    Test Patir::PatirLoggerFormatter

---
+++ test/test_patir_patirloggerformatter.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+
+require 'minitest/autorun'
+require 'patir/base'
+
+module Patir::Test
+  ##
+  # Check Patir::PatirLoggerFormatter
+  class PatirLoggerFormatter < Minitest::Test
+    ##
+    # Verify that Patir::PatirLoggerFormatter#call correctly formats messages
+    def test_call
+      formatter = Patir::PatirLoggerFormatter.new
+      time = Time.new(2021, 1, 19, 21, 59, 14)
+      assert_equal("[20210119 21:59:14]     1: Ouch\n",
+                   formatter.call(Logger::INFO, time, 'test_prog', 'Ouch'))
+      time = Time.new(2021, 1, 19, 22, 4, 11)
+      assert_equal("[20210119 22:04:11]     2: Oh oh\n",
+                   formatter.call(Logger::WARN, time, 'test_prog', 'Oh oh'))
+    end
+
+    ##
+    # Verify that Patir::PatirLoggerFormatter is being initialized correctly
+    def test_initialization
+      formatter = Patir::PatirLoggerFormatter.new
+      assert_equal('%Y%m%d %H:%M:%S', formatter.datetime_format)
+    end
+  end
+end
